b2f9f72581461476fec0942a4a7e1b749e3b1da5
Berke Gökçe <69328410+berkegokce@users.noreply.github.com>
2021-06-30 00:56:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Client();
-const request = require('request');
+const request = require("request");
 
 let ayarlar = {
-    "özelurl": "url değişince yapılcak url",
-    "urlog": "guard log",
-    "token": "bot token" // clasus#0001
-}
+  özelurl: "url değişince yapılcak url",
+  urlog: "guard log",
+  token: "bot token", 
+};
 
-client.on('guildUpdate', async (berke, gokce) => {
-    if (berke.vanityURLCode === gokce.vanityURLCode) return;
-    let entry = await gokce.fetchAuditLogs({
-        type: 'GUILD_UPDATE'
-    }).then(audit => audit.entries.first());
-    if (!entry.executor || entry.executor.id === client.user.id) return;
-    let channel = client.channels.cache.get(Options.urlog);
+client.on("guildUpdate", async (berke, gokce) => {
+  if (berke.vanityURLCode === gokce.vanityURLCode) return;
+  let entry = await gokce
+    .fetchAuditLogs({
+      type: "GUILD_UPDATE",
+    })
+    .then(audit => audit.entries.first());
+  if (!entry.executor || entry.executor.id === client.user.id) return;
+  let channel = client.channels.cache.get(Options.urlog);
 
-    if (channel) channel.send(`
+  if (channel)
+    channel.send(`
     ${entry.executor} adlı kullanıcı özel url'yi değiştirmeye çalıştı ve sunucudan yasaklandı.
-    `)
+    `);
 
-    if (!channel) gokce.owner.send(`
+  if (!channel)
+    gokce.owner.send(`
     ${entry.executor} adlı kullanıcı özel url'yi değiştirmeye çalıştı ve sunucudan yasaklandı.
-    `)
+    `);
 
-    gokce.members.ban(entry.executor.id, {
-        reason: `${entry.executor.tag} URL GUARD`
-    });
+  gokce.members.ban(entry.executor.id, {
+    reason: `${entry.executor.tag} URL GUARD`,
+  });
 
-    const ayarlar = {
-        url: `https://discord.com/api/v6/guilds/${gokce.id}/vanity-url`,
-        body: { code: ayarlar.özelurl },
-        json: true,
-        method: 'PATCH',
-        headers: { "Authorization": `Bot ${ayarlar.token}` }
-    };
+  const ayarlar = {
+    url: `https://discord.com/api/v6/guilds/${gokce.id}/vanity-url`,
+    body: { code: ayarlar.özelurl },
+    json: true,
+    method: "PATCH",
+    headers: { Authorization: `Bot ${ayarlar.token}` },
+  };
 
-    request(ayarlar, (err, res, body) => {
-        if (err) {
-            return console.log(err);
-        }
-    });
+  request(ayarlar, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+  });
 });
